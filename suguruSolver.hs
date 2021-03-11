b47f0fb90ad4d2287117bcdc9390d76c272067f4
Felipe Santos <fe.campossantos@gmail.com>
2021-03-11 16:45:16 -0300

    started implementing after the first possibility

--- suguruSolver.hs
+++ suguruSolver.hs
@@ -66,7 +66,117 @@ module SuguruSolver(fillAreas1, getPossibilitiesArea, getPossibilitiesMatrix, cl
             else
                 (delete n (area!!count)) : (removeNumberFromOtherCells area n idx size (count+1))
 
+---------------------------------------------------------------------------------------------------------------------------------------------
+
+    {- Chama o checkSur area por área
+    checkSurroundings ::    board     -> possibilities ->   suguru   -> loop count -> possibilities-}
+    checkSurroundings :: [(Int, Int)] ->    [[[Int]]]  ->  [[[Int]]] ->     Int    ->    [[[Int]]]
+    checkSurroundings board possibilities suguru loop = --indexArea vai ser zero na chamada do checkSur
+        if loop == (length board) then []
+        else
+            (checkSur board possibilities suguru loop 0) : (checkSurroundings board possibilities suguru (loop+1))
+
+    -- tem que retornar as possibilities inteiras, não só a da área
+    checkSur board poss suguru indexArea loop = 
+        if loop == (length (board!!indexArea)) then [] -- se tiver finalizado as células da área, pare
+        else
+            if (suguru!!(fst (board!!indexArea)!!loop))!!(snd (board!!indexArea)!!loop) == 0 then --se no tabuleiro a célula dessa área conter zero
+                {- se o elemento suguru(x,y) (onde x e y vem dos elementos das áreas) for == 0, pegaremos
+                os elementos em volta dele e caso algum deles seja diferente de zero vamos retirar
+                essa possibilidade do elemento 0
+                -}
+
+                {-                                    (em poss tamb)   (em poss tamb)
+                                                    #area na board| #elemento na area| board | suguru | possibilities-}
+                removeAroundPossibilitiesFromElement     indexArea          loop       board   suguru   possibilities
+            else -- a celula nao tem zero, entao vamos remover o valor dela das possibilidades ao redor dela
+
+
     {-
-    checkSurroundings ::    board     -> possibilities -> possibilities-}
-    checkSurroundings :: [(Int, Int)] ->    [[[Int]]]  ->    [[[Int]]]
-    checkSurroundings
+    Element places example
+    ------------------------------------
+    |           |          |           |
+    |upper left |  upper   |upper right|
+    |           |  central |           |
+    ------------------------------------
+    |           |          |           |
+    |left wall  |  central |right wall |
+    |    -      |     -    |      -    |
+    ------------------------------------
+    |     -     |     -    |     -     |
+    |   lower   |   lower  |  lower    |
+    |   left    |  central |   right   |
+    ------------------------------------
+
+    -}
+    removeAroundPossibilitiesFromElement
+    removeAroundPossibilitiesFromElement idxArea idxElem board suguru poss = 
+        -- row -> (fst ((board!!idxArea)!!idxElem))
+        -- col -> (snd ((board!!idxArea)!!idxElem))
+        -- board size -> (length suguru) (NxN)
+        if (fst ((board!!idxArea)!!idxElem)) > 0 then -- row > 0
+            if (fst ((board!!idxArea)!!idxElem)) < (length suguru) then  -- row < size
+                if (snd ((board!!idxArea)!!idxElem)) > 0 then -- col > 0
+                    if (snd ((board!!idxArea)!!idxElem)) < (length suguru) then -- col < size
+                        removeFromCentral (fst ((board!!idxArea)!!idxElem)) (snd ((board!!idxArea)!!idxElem)) idxArea idxEleme board poss suguru
+                    else
+                        removeFromRightWall
+                else
+                    removeFromLetfWall
+            else
+                if (snd ((board!!idxArea)!!idxElem)) > 0 then  -- col > 0
+                    if (snd ((board!!idxArea)!!idxElem)) < (length suguru) then -- col < size
+                        removeFromLowerCentral
+                    else
+                        removeFromLowerRight
+                else
+                    removeFromLowerLeft
+        else
+            if (snd ((board!!idxArea)!!idxElem)) > 0 then  -- col > 0
+                if (snd ((board!!idxArea)!!idxElem)) < (length suguru) then  -- col < size
+                    removeFromUpperCentral
+                else
+                    removeFromUpperRight
+            else
+                removeFromUpperLeft
+
+    {-
+        area -> number of the area in the board matrix.
+        element -> index of the element inside the area array
+        row -> row of the cell in the suguru
+        col -> col of the cell in the suguru
+        suguru -> suguru board to check the other values
+    -}
+    removeFromCentral row col idxArea idxElem board poss suguru =
+        -- value on the upper left
+        if length (getValueFrom (row-1) (col-1) board) == 1 then
+            removeFromPossibilities (getValueFrom (row-1) (col-1) board) idxArea idxElem poss 0 0
+        -- value on the upper
+        if length (getValueFrom (row-1) (col) board) == 1 then
+            removeFromPossibilities (getValueFrom (row-1) (col) board) idxArea idxElem poss 0 0
+        -- value on the upper right
+        if length (getValueFrom (row-1) (col+1) board) == 1 then
+                removeFromPossibilities (getValueFrom (row-1) (col+1) board) idxArea idxElem poss 0 0
+        -- value on the left
+        if length (getValueFrom (row) (col-1) board) == 1 then
+                removeFromPossibilities (getValueFrom (row) (col-1) board) idxArea idxElem poss 0 0
+        -- value on the right
+        if length (getValueFrom (row-1) (col+1) board) == 1 then
+                removeFromPossibilities (getValueFrom (row-1) (col+1) board) idxArea idxElem poss 0 0
+        -- value on the lower left
+        if length (getValueFrom (row+1) (col-1) board) == 1 then
+                removeFromPossibilities (getValueFrom (row+1) (col-1) board) idxArea idxElem poss 0 0
+        -- value on the lower
+        if length (getValueFrom (row+1) (col) board) == 1 then
+                removeFromPossibilities (getValueFrom (row+1) (col) board) idxArea idxElem poss 0 0
+        -- value on the lower right
+        if length (getValueFrom (row+1) (col+1) board) == 1 then
+                removeFromPossibilities (getValueFrom (row+1) (col+1) board) idxArea idxElem poss 0 0
+    
+    getValueFrom row col board = (board!!row)!!col
+
+    removeFromPossibilities value idxArea idxElem (a:b:c) {-poss-} loopArea loopElem = 
+        if loopArea == idxArea then
+            a : (delete value b!!idxElem) : c
+        else
+            a : removeFromPossibilities value idxArea idxElem (b:c) {-poss-} loopArea loopElem
