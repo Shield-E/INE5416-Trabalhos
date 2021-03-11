4777190feab5336bedd26ea4486529971e99303d
Felipe Santos <fe.campossantos@gmail.com>
2021-03-11 13:23:41 -0300

    finished implementing the possibility arrays creation

--- main.hs
+++ main.hs
@@ -5,24 +5,88 @@ main = do
     -- print("Cria uma matriz predefinida")
     -- let m = createMatrix [[1,2,3],[4,5,6],[7,8,9]]
     -- print(m)
+    {-  Aqui são definidas duas matrizes:
+        1. A matriz do tabuleiro SUGURU, com os valores preenchidos sendo seus valores e os não preenchidos sendo zero
+        2. A matriz de áreas do tabuleiro, com tuplas representando quais células fazem parte daquela área
+    -}
     let suguru = [  [1,0,0,5,0],
                     [0,0,0,0,0],
                     [1,0,2,0,4],
                     [0,0,0,0,0],
                     [0,3,0,0,0]]
-    let board = [   [1,2,3,6],
-                    [4,5,10,15,20],
-                    [7,8,11,12,16],
-                    [9,13,14,17,18],
-                    [19,21,22,23,24],
-                    [25]]
     
-    let board2 = [  [(1,1),(1,2),(1,3),(2,1)],
+    let board = [  [(1,1),(1,2),(1,3),(2,1)],
                     [(1,4),(1,5),(2,5),(3,5),(4,5)],
                     [(2,2),(2,3),(3,1),(3,2),(4,1)],
                     [(2,4),(3,3),(3,4),(4,2),(4,3)],
                     [(4,4),(5,1),(5,2),(5,3),(5,4)],
                     [(5,5)]]
-    
-    print(suguru)
-    print(fillAreas1 board2 suguru)
+
+    {- Aqui o programa checa se existe alguma área de apenas uma celula, e se existir preenche ela com 1 (seu único valor possível)
+    -}
+    let suguru2 = fillAreas1 board suguru
+    print(suguru2)
+    print()
+    print()
+    {- O programa então cria uma nova matriz auxiliar: a matriz de possibilidades. O formato tem a mesma estrutura da de áreas, para
+        que seja possível correlacionar os endereços das células das áreas com seus possíveis valores.
+        Ex: se o elemento m3x3 da matriz de áreas é (5,7), então a celula s5x7 do tabuleiro pode ter os valores da lista
+        l3x3 da matriz de possibilidades.
+
+        Nesse ponto ainda existem valores que não são uma possibilidade nas listas. O que a função faz é pegar o tamanho
+        da área e colocar como possibilidades os bounds do tamanho para cada célula. Ex: se a área tem 5 quadrados,
+        então seus elementos estão em [1,5]. Mesmo que ja exista o elemento 4 nessa área;
+    -}
+    let possibilities = getPossibilitiesMatrix board suguru2
+    print(possibilities)
+    {-
+    [
+        [[1],[1,2,3,4],[1,2,3,4],[1,2,3,4]],
+        [[5],[1,2,3,4,5],[1,2,3,4,5],[4],[1,2,3,4,5]],
+        [[1,2,3,4,5],[1,2,3,4,5],[1],[1,2,3,4,5],[1,2,3,4,5]],
+        [[1,2,3,4,5],[2],[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5]],
+        [[1,2,3,4,5],[1,2,3,4,5],[3],[1,2,3,4,5],[1,2,3,4,5]],
+        [[1]]
+    ]
+    -}
+    print()
+    print()
+
+    {- Agora é feita a correção da função acima. Usando do mesmo exemplo: uma área de 5 quadrados ja tem o elemento 4, então é retirada
+    essa possibilidade das listas de possibilidades das outras celulas dessa área. Supondo que essa área esteja no seguinte formato:
+    [0,0,4,0,0], suas possibilidades serão agora -> [ [1,2,3,5], [1,2,3,5], [4], [1,2,3,5], [1,2,3,5] ]
+    -}
+    let possibilites2 = clearPossibilities possibilities
+    print(possibilites2)
+    {-
+    [
+        [[1],[2,3,4],[2,3,4],[2,3,4]],
+        [[5],[1,2,3],[1,2,3],[4],[1,2,3]],
+        [[2,3,4,5],[2,3,4,5],[1],[2,3,4,5],[2,3,4,5]],
+        [[1,3,4,5],[2],[1,3,4,5],[1,3,4,5],[1,3,4,5]],
+        [[1,2,4,5],[1,2,4,5],[3],[1,2,4,5],[1,2,4,5]],
+        [[1]]
+    ]
+    -}
+    print()
+    print()
+    {- TO DO -> COLOCAR AQUI UMA FUNÇÃO QUE PASSA NOVAMENTE POR TODAS AS POSSIBILIDADES E SE HOUVER UMA LISTA COM APENAS UMA POSSIBILIDADE,
+    JA COLOCAR ELA NO TABULEIRO. ISSO PODE ECONOMIZAR UM TEMPINHO NA PRÓXIMA FUNÇÃO
+    -}
+
+    {- Aqui começa a ser feita as operações fora de áreas.
+        1. Pegaremos célula a célula do tabuleiro
+            1.1 Se ela estiver preenchida com zero, checaremos a localização dela para vermos quais células ao redor dela existem, e 
+            checaremos essas celulas para ver se alguma já esta preenchida. Caso encontremos uma preenchida, vamos tirar o valor dessa
+            preenchida da lista de possibilidades da célula analisada.
+            1.2 Se ela estiver preenchida com qualquer coisa diferente de zero, vamos ver quais células ao redor dela existem,
+            e dessas vamos tirar da lista de possibilidades o valor da célula analisada
+    -}
+
+    {- TO DO -> COLOCAR AQUI UMA FUNÇÃO QUE PASSA NOVAMENTE POR TODAS AS POSSIBILIDADES E SE HOUVER UMA LISTA COM APENAS UMA POSSIBILIDADE,
+    JA COLOCAR ELA NO TABULEIRO. ISSO PODE ECONOMIZAR UM TEMPINHO NA PRÓXIMA FUNÇÃO
+    -}
+
+    {-
+    AQUI PODEMOS OU REPETIR OS PASSOS ANTERIORES PARA TENTAR MATAR MAIS ALGUMAS CELULAS COM VALOR UNICO OU JA COMEÇAR O TENTATIVA E ERRO
+    -}
--- suguruSolver.hs
+++ suguruSolver.hs
@@ -1,14 +1,72 @@
-module SuguruSolver(fillAreas1) where
+module SuguruSolver(fillAreas1, getPossibilitiesArea, getPossibilitiesMatrix, clearPossibilities) where
 
     import Matrix
+    import Data.List -- (delete 10 [0,0,10] removes the 10 ; delete 2 [0,1,10] does nothing without error)
 
+    {- Fill all the areas that have size 1 with the number 1 (just that possibility)-}
     fillAreas1 :: [[(Int,Int)]] -> [[Int]] -> [[Int]]
-    fillAreas1 board suguru = fillAreas1Aux board suguru
-
-    fillAreas1Aux :: [[(Int,Int)]] -> [[Int]] -> [[Int]]
-    fillAreas1Aux [] m = m
-    fillAreas1Aux (x:y) (a:b) = 
+    fillAreas1 [] m = m
+    fillAreas1 (x:y) (a:b) = 
         if length x == 1 then
-            fillAreas1Aux y (put (a:b) (x!!0) 1)
+            fillAreas1 y (put (a:b) (x!!0) 1)
         else
-            fillAreas1Aux y (a:b)
+            fillAreas1 y (a:b)
+    
+    {- returns a 3d matrix with all the possibilites of numbers for each cell, using the size of the area;
+    -> calls getPossibilitiesArea for each area of the board
+    !!! not final possibilities, only fills with array of [1 -> size], but doesnt see if any other cell is already filled
+    getPossibilitiesMatrix :: board          -> suguru  -> R: matriz de possibilidades-}
+    getPossibilitiesMatrix :: [[(Int, Int)]] -> [[Int]] -> [[[Int]]]
+    getPossibilitiesMatrix [] _ = []
+    getPossibilitiesMatrix (x:y) matriz =
+        (getPossibilitiesArea x matriz ((length x)-1) (length x)) : (getPossibilitiesMatrix y matriz)
+
+    {-returns a 2d matrix, containing an array of possibilites for each cell in the received area
+    !!! same of the getPossibilitiesMatrix applies here
+    getPossibilitiesArea :: board        -> suguru  -> loopCount -> tamanhoarea -> R: matriz de possibilidades -}
+    getPossibilitiesArea :: [(Int, Int)] -> [[Int]] -> Int       -> Int          -> [[Int]]
+    getPossibilitiesArea _ _ (-1) _ = []
+    getPossibilitiesArea (l1:l2) matriz n tamanhoArea = 
+        if (matriz!!((fst l1)-1))!!((snd l1)-1) == 0 then
+            -- preenche com uma lista de [1..length area]
+            [1..tamanhoArea] : (getPossibilitiesArea l2 matriz (n-1) tamanhoArea)
+        else
+            -- coloca uma lista com o própio elemento na lista
+            [(matriz!!((fst l1)-1))!!((snd l1)-1)] : (getPossibilitiesArea l2 matriz (n-1) tamanhoArea)
+    
+
+    {- runs through each area, removing from the possibilities the numbers that
+    are already present in that area
+    clearPossibilities :: possibilites  -> possibilities -}
+    clearPossibilities ::   [[[Int]]]   ->   [[[Int]]]
+    clearPossibilities [] = []
+    clearPossibilities (a:b) = (clearArea a (length a) 0) : (clearPossibilities b)
+
+    {-
+    clearArea :: possibilities of area -> area size  -> loop -> possibilites-}
+    clearArea ::      [[Int]]         ->     Int     ->  Int ->  [[Int]]
+    clearArea pos size loop =
+        if loop == size then pos
+        else 
+            if length (pos!!loop) == 1 then
+                -- if the possibilities for that cell is only one (already correct) then remove this number from all the OTHER cells
+                clearArea (removeNumberFromOtherCells pos ((pos!!loop)!!0) loop size 0) size (loop+1)
+            else
+                -- if not, keep this cell intact and continue
+                clearArea pos size (loop+1)
+    
+    {-
+    removeNumberFromOtherCells ::  cells  -> number -> index to dont change -> area size -> loop count -> cells  -}
+    removeNumberFromOtherCells :: [[Int]] ->   Int  ->          Int         ->    Int    ->     Int    -> [[Int]]
+    removeNumberFromOtherCells area n idx size count = 
+        if count == size then []
+        else
+            if count == idx then
+                area!!count : removeNumberFromOtherCells area n idx size (count+1)
+            else
+                (delete n (area!!count)) : (removeNumberFromOtherCells area n idx size (count+1))
+
+    {-
+    checkSurroundings ::    board     -> possibilities -> possibilities-}
+    checkSurroundings :: [(Int, Int)] ->    [[[Int]]]  ->    [[[Int]]]
+    checkSurroundings
